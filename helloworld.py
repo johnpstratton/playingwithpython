a7eff8b2942d2eec9c37607b6bb0a88f21ec5f89
John_Stratton <john.p.stratton@gmail.com>
2020-05-18 12:33:58 -0700

    Linter issued fixed

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print('Hello World')
+print()
+print('Did you see that blank line?')
+print('Blank line \nin the middle of string')
+print('Adding numbers')
+x = 42 + 206
+print('Performing division')
+y = x / 0
+print('Math complete')
