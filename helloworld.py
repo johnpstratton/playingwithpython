5094fe9a362ee9b98d4edd12432b73ff646b1ed5
John_Stratton <john.p.stratton@gmail.com>
2020-05-18 14:16:14 -0700

    fixed ssh key

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,14 @@ print('Did you see that blank line?')
 print('Blank line \nin the middle of string')
 print('Adding numbers')
 x = 42 + 206
-print('Performing division')
-y = x / 0
-print('Math complete')
+print()
+first_name = 'John'
+last_name = 'Stratton'
+output = 'Hello, ' + first_name + ' ' + last_name
+print(output)
+print()
+output = 'Hello, {0} {1}'.format(first_name, last_name)
+print(output)
+print()
+days_in_feb = 28
+print(str(days_in_feb) + 'in February')
