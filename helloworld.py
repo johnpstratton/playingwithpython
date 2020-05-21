f1bf649bc547ce692e60c99923be20b2217ede45
John_Stratton <john.p.stratton@gmail.com>
2020-05-21 08:52:56 -0700

    Notes from today

--- helloworld.py
+++ helloworld.py
@@ -19,9 +19,11 @@
 # first_num = '5'
 # second_num = '6'
 # print(int(first_num) + int(second_num))
-from datetime import datetime
-current_date = datetime.now()
-print('Today is: ' + str(current_date))
+
+#################################### Printing datetime by importing and using datetime library ############
+# from datetime import datetime
+# current_date = datetime.now()
+# print('Today is: ' + str(current_date))
 
 ################################### Data Conversion ##################################
 
@@ -29,10 +31,13 @@ print('Today is: ' + str(current_date))
 # convert the temperature to Farenheit, and 
 # print out the converted temperature
 
-tempInC = input('What is the temperature in (in C!): ') #ask the user for temp in C
-tempInC = float(tempInC) # then convert the value from the string to float
-tempInF = tempInC * 9/5 + 32 # convert the temperature to Farenheit
-print(tempInF) # print out the converted temperature
-print('{0} C = {1} F'.format(tempInC, tempInF))
+# tempInC = input('What is the temperature in (in C!): ') #ask the user for temp in C
+# tempInC = float(tempInC) # then convert the value from the string to float
+# tempInF = tempInC * 9/5 + 32 # convert the temperature to Farenheit
+# print(tempInF) # print out the converted temperature
+# print('{0} C = {1} F'.format(tempInC, tempInF))
 
-
+############################## Data Conversion with Input from user #########
+num1 = int( input('Enter a number: '))
+num2 = int( input('Enter another number: '))
+print(num1 + num2)
